15dbc4545b482b4cd0adf9cecbb620c9cb5bfcf2
Mr-Tang <1101087566@qq.com>
2019-08-23 00:44:50 +0800

    create project

---
+++ src/main/java/com/newer/github20190822/Github20190822Application.java
@@ -0,0 +1,13 @@
+package com.newer.github20190822;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github20190822Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github20190822Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/newer/github20190822/Github20190822ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newer.github20190822;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Github20190822ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
